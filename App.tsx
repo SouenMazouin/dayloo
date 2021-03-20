4040f7312300c22e4ab98564d9f22255421c443d
souen.mazouin <souen.mazouin@protonmail.com>
2021-03-20 14:42:35 +0100

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import { SafeAreaView, ScrollView, View } from 'react-native';
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>Initial Commit</View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
