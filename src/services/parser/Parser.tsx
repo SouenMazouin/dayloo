30b7e01ab1b37697c3c9707bb9783ded4b3d85af
souen.mazouin <souen.mazouin@protonmail.com>
2021-04-18 16:37:50 +0200

    add: JSdoc for TagParser function

--- src/services/parser/Parser.tsx
+++ src/services/parser/Parser.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { Text } from 'react-native';
 import MathView from 'react-native-math-view';
 
+/**
+ * Return parsed texts for Items of CardSwiper with base styles defined from JSON data's
+ * It is also possible to inject custom styles at the time of the function call
+ * @param   {string} element Text element we want to render, is generally passed from Props
+ * @param   { {[key: string]: string | number} } customStyles Standard CSS StyleSheet Object, eg. {color: 'red', fontSize: 10}
+ * @return  {<Text style={[{tagProperty: 'tagValue'}, customStyles]}>{parsedElement}</Text> | <MathView math={latexElement} />} Parsed element or Latex element with proper styles
+ */
+
 export function TagParser(
   element: string,
   customStyles: { [key: string]: string | number },
