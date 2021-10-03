551a7e25c20649ef833edf831bce906fb10bddc6
Souen Mazouin <souen.mazouin@gmail.com>
2021-10-03 12:25:41 +0200

    change: improve styles to strings object parser

--- src/services/parsers/ObjectToString.ts
+++ src/services/parsers/ObjectToString.ts
@@ -1,10 +1,7 @@
 import { injectedStyle } from '../../shared/@types/types';
 
 export function ObjectStyleToString(obj: injectedStyle): string {
-  return (
-    Object.entries(obj)
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      .map(([key, value]) => `${key}: ${value}`)
-      .join(', ')
-  );
+  return Object.entries(obj)
+    .map(([key, value]) => `${key}: ${JSON.stringify(value)}`)
+    .join(', ');
 }
